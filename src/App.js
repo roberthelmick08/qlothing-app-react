13a077e5e5e13f0ce94d9947906cca1c232c60bd
Robert Helmick <robert.helmick.08@gmail.com>
2023-09-14 16:13:42 -0600

    routing & navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from "./components/navigation/navigation.component";
+import Home from "./routes/home/home.component";
+import { Routes, Route, Outlet } from 'react-router-dom';
+import SignIn from "./routes/sign-in/sign-in.component";
 
-function App() {
+const Shop = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Shop Page</p>
+      <Outlet />
     </div>
+  )
+}
+const App = () => {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Route>
+    </Routes>
   );
 }
 
